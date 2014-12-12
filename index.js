4c5f2b31c54ac60f6170d8231d2de9a90f8e15a6
Kris Hagel <kris@hatrackgroup.com>
2014-12-12 09:36:36 -0800

    Adding moment.js to parse dates on callss coming in.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var winston = require('winston');
 require('winston-syslog').Syslog;
 var options = {
@@ -6,13 +7,15 @@ var options = {
 }
 winston.add(winston.transports.Syslog, options);
 
+var current_year = moment().year();
+
 var s = require('net').Socket();
 s.connect(1752, '10.0.240.29');
 s.on('data', function(d){
   var raw = d.toString('utf8');
-  var log = {
-    date: raw.substring(1,6),
-    start_time: raw.substring(7,12),
+  var raw_time = raw.substring(1,6) + '-' + current_year + ' ' + raw.substring(7,12);
+  var json = {
+    timestamp: moment(raw_time, "MM/DD-YYYY HH:mm").format(),
     duration: raw.substring(14,22),
     calling_party: raw.substring(23,27),
     time_to_answer: raw.substring(29,32),
@@ -22,8 +25,15 @@ s.on('data', function(d){
     called_party: raw.substring(61,65),
     transfer: raw.substring(65,66),
     third_party: raw.substring(67,71),
-    account_code: raw.substring(72,84)
-  }
-  console.log(log);
+    account_code: raw.substring(72,84),
+    route_optimization: raw.substring(84,85),
+    system_identifier: raw.substring(85,88),
+    ani: raw.substring(91,101),
+    dnis: raw.substring(102,112),
+    call_indentifier: raw.substring(113,121),
+    call_sequence_identifier: raw.substring(121,122),
+    associated_call_identifier: raw.substring(123,131)
+  }  
+  console.log(json);
   winston.info(d.toString());
 });
