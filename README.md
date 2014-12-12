10e2665a2ae41566c8daa17fa85cf8d841bf2945
Kris Hagel <kris@hatrackgroup.com>
2014-12-12 13:46:41 -0800

    Moved server addresses into environment variables and updated the readme and package.json files

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ mitel-syslog
 ============
 
 mitel controller log to syslog server daemon
+
+Install
+
+    Prerequisits:
+
+    * Node.js
+
+To download software and install dependencies, please run the following commands (changing the ip addresses to match your systems):
+
+<code>
+    git clone https://github.com/psd401/mitel-syslog
+    cd mitel-syslog
+    npm install
+    npm install -g forever
+    MITEL_HOST=127.0.0.1 SYSLOG_HOST=127.0.0.1 forever index.js
+</code>
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var moment = require('moment');
 var winston = require('winston');
 require('winston-syslog').Syslog;
+
 var options = {
-  host: '10.0.0.153',
+  host: process.env.SYSLOG_HOST,
   port: 5002
 }
+
 winston.add(winston.transports.Syslog, options);
 
 var current_year = moment().year();
 
 var s = require('net').Socket();
-s.connect(1752, '10.0.240.29');
+s.connect(1752, process.env.MITEL_HOST);
 s.on('data', function(d){
   var raw = d.toString('utf8');
   var raw_time = raw.substring(1,6) + '-' + current_year + ' ' + raw.substring(7,12);
@@ -33,7 +35,11 @@ s.on('data', function(d){
     call_indentifier: raw.substring(113,121),
     call_sequence_identifier: raw.substring(121,122),
     associated_call_identifier: raw.substring(123,131)
-  }  
-  console.log(json);
-  winston.info(d.toString());
+  }
+  var log = moment(raw_time, "MM/DD-YYYY HH:mm").format()+','+raw.substring(14,22)+','+raw.substring(23,27)+','+
+    raw.substring(29,32)+','+raw.substring(33,59)+','+raw.substring(59,60)+','+raw.substring(60,61)+','+
+    raw.substring(61,65)+','+raw.substring(65,66)+','+raw.substring(67,71)+','+raw.substring(72,84)+','+
+    raw.substring(84,85)+','+raw.substring(85,88)+','+raw.substring(91,101)+','+raw.substring(102,112)+','+
+    raw.substring(113,121)+','+raw.substring(121,122)+','+raw.substring(123,131)
+  winston.info(log);
 });
